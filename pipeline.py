0a2232aad9e745e57b68934ab4e07624473e1e69
Madina Seidualy <s1990277@bioinfmsc5.bio.ed.ac.uk>
2019-11-12 00:06:27 +0000

    message for errors are included

--- pipeline.py
+++ pipeline.py
@@ -5,6 +5,17 @@ import string, re
 import collections
 import numpy as np
 
+
+def error_msg():
+	for i in range(20):
+		print("==",end="")
+	print("ERROR", end="")
+	for i in range(20):
+		print("==", end="")
+	print("\n")
+	return (0)
+		
+
 def check_valid(name):
 	count = subprocess.check_output('esearch -db protein -query "{0}" | xtract -pattern ENTREZ_DIRECT -element Count'.format(name),shell=True)
 	if int(count) == 0:
@@ -20,10 +31,12 @@ def check_both_input(name,protein):
 def check_input(name):
 	valid = re.compile("[A-Za-z0-9- ]+")
 	if name.isdigit() == True or valid.fullmatch(name) == None or re.search(r'^[\s\n\t]', name):
-		print("The name "+name+" is invalid. Special characters or name with only numbers or name starting with whitespaces are not welcomed!")
+		error_msg()
+		print("The name "+name+" is invalid. Special characters, Only numbers, Whitespaces starts strongly restricted!\n")
 		return 0
 	if check_valid(name) != 1:
-		print("In database there is no imformation about "+name+". The name might have mispelling. Please try again!")
+		error_msg()
+		print("In database there is no imformation about "+name+". The name might have mispelling. Please try again!\n")
 		return 0
 	return 1
 
@@ -40,15 +53,16 @@ def get_input():
 def begin():
 	name, protein = get_input()
 	while check_both_input(name, protein) != 1:
-		print("No data found with a taxonomic group name \""+name+"\" and protein family name \""+protein+"\" in database. Try one more time with the valid names!")
+		error_msg()
+		print("No data found with a taxonomic group name \""+name+"\" and protein family name \""+protein+"\" in database. Try one more time with the valid names!\n")
 		name, protein = get_input()
 	rename = name.replace(" ","_")
 	reprotein = protein.replace(" ","_")
-	subprocess.call('esearch -db protein -query "{0}[prot] NOT PARTIAL" -organism "{1}" | efetch -db protein -format docsum | xtract -pattern DocumentSummary -element Organism > {2}_seq_species_{3}.txt'.format(protein, name, reprotein,rename), shell=True)
-	my_list = open('{0}_seq_species_{1}.txt'.format(reprotein, rename)).readlines()
+	subprocess.call('esearch -db protein -query "{0}[prot] NOT PARTIAL" -organism "{1}" | efetch -db protein -format docsum | xtract -pattern DocumentSummary -element Organism > {2}_organism_list_{3}.txt'.format(protein, name, rename,reprotein), shell=True)
+	my_list = open('{0}_organism_list_{1}.txt'.format(rename, reprotein)).readlines()
 	count = len(my_list)
 	species = len(set(my_list))
-	print('The search with your input resulted in '+str(count)+' sequences from '+str(species)+' species.')
+	print('The search with your input resulted in '+str(count)+' sequences from '+str(species)+' species.\n')
 	reply = input('Do you want to continue the process or change the input? yes/no\n').upper()
 	return (reply, name, protein)
 
@@ -69,10 +83,16 @@ while not (re.search('[YES]|[Y]',reply)):
 	print("We are going back to the beginning!")
 	rename = name.replace(" ","_")
 	reprotein = protein.replace(" ","_")
-	os.remove("{0}_seq_species_{1}.txt".format(reprotein, rename))
+	os.remove("{0}_organism_list_{1}.txt".format(rename, reprotein))
 	reply, name, protein = begin()
 	
 print("All sequences are being downloaded...")
 rename = name.replace(" ","_")
 reprotein = protein.replace(" ","_")
-subprocess.call('esearch -db protein -query "{0}[prot] NOT PARTIAL" -organism "{1}" | efetch -db protein -format fasta > {2}_{3}.fasta'.format(protein, name, reprotein, rename), shell=True)
+subprocess.call('esearch -db protein -query "{0}[prot] NOT PARTIAL" -organism "{1}" | efetch -db protein -format fasta > {2}_{3}.fasta'.format(protein, name, rename, reprotein), shell=True)
+subprocess.call('clustalo -i {0}_{1}.fasta -o {0}_{1}_aln.fasta -v'.format(rename, reprotein), shell =True)
+print("Multiple Alignment for downloaded protein sequences is done")
+subprocess.call('cons -sequence {0}_{1}_aln.fasta -outseq {0}_{1}_consensus.fasta -auto'.format(rename, reprotein), shell=True)
+print("Program has created a consensus sequence from a multiple alignment")
+
+#subprocess.call('makeblastdb -in {0}_{1}')
