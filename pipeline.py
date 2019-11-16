df6361453c4b900a694f9fa2cff17d36d32e7376
Madina Seidualy <s1990277@bioinfmsc5.bio.ed.ac.uk>
2019-11-16 21:57:26 +0000

    secondary struct added plot needed

--- pipeline.py
+++ pipeline.py
@@ -109,7 +109,7 @@ print("Multiple Alignment for downloaded protein sequences is done")
 subprocess.call('cons -sequence {0}_{1}_aln.fasta -outseq {0}_{1}_consensus.fasta -auto'.format(rename, reprotein), shell=True)
 print("Program has created a consensus sequence from a multiple alignment")
 
-subprocess.call('makeblastdb -in {0}_{1}.fasta -dbtype prot -out {0}_{1}_db'. format(rename, reprotein), shell=True)
+subprocess.call('makeblastdb -in {0}_{1}.fasta -dbtype prot -out {0}_{1}_db'.format(rename, reprotein), shell=True)
 print("From all fasta sequences the program created database for blast alingment\n")
 subprocess.call('blastp -db {0}_{1}_db -query {0}_{1}_consensus.fasta -outfmt 7 -max_hsps 1 > {0}_{1}_similarity_seq_blast.out'.format(rename, reprotein), shell = True)
 print("Alighned all sequences in BLAST and their HSP score saved in new file\n")
@@ -138,15 +138,15 @@ subprocess.call('eog plotcon.svg&', shell = True)    #'eog' used for presenting 
 
 
 #for calling my files I need to put accession numbers as a name for each files
-fasta_file = open("{0}_{1}.fasta".format(rename, reprotein)).read().rstrip()
-os.mkdir('{0}/fast_files'.format(inside_dir))
-os.chdir('{0}/fast_files'.format(inside_dir))
+fasta_all = open("{0}_{1}.fasta".format(rename, reprotein)).read().rstrip()
+os.mkdir('{0}/FASTA_FILES'.format(inside_dir))
+os.chdir('{0}/FASTA_FILES'.format(inside_dir))
 pat_out = ('{0}/PATMOTIFS_OUT'.format(inside_dir))
 os.mkdir(pat_out)
 found_motif = open('{0}/FOUND_MOTIFS.txt'.format(inside_dir),"w")
 found_motif.write("Accession number\tMotif name\n")
 motif_array = []
-each_seq = fasta_file.split(">")
+each_seq = fasta_all.split(">")
 for seq in each_seq:
 	for keys in access_hsp.keys():
 		if re.match(keys, seq):
@@ -155,7 +155,7 @@ for seq in each_seq:
 			sep_file.close()
 			subprocess.call("patmatmotifs -sequence {0}.fasta -outfile {1}/{0}.patmatmotifs -full".format(keys, pat_out), shell=True)
 			pat_file = open("{0}/{1}.patmatmotifs".format(pat_out, keys)).readlines()
-			for line in pat_file:			
+			for line in pat_file:
 				if re.search('#',line):
 					next
 				elif re.search('Motif', line):
@@ -163,10 +163,36 @@ for seq in each_seq:
 					index = line.find("=")
 					motif = line[index+2:]
 					motif_array.append(motif)
-					found_motif.write('{0}\t{1}'.format(keys,motif))			
+					found_motif.write('{0}\t{1}'.format(keys,motif))
 found_motif.close()
 print("The output of all sequences' scanning for motifs from the PROSITE database are saved in the folder by name PATMOTIFS_OUT inside the  working directory\n")
 print("In subset of protein sequences {0} known motifs were associated with {1} protein sequences. In FOUND_MOTIFS.txt file you can find list of accesiion numbers with found motif names\n".format(len(set(motif_array)),len(motif_array)))
 print("Found MOTIF names are: ")
 print("\n".join(set(motif_array)))
 
+fasta_files = os.listdir()
+sec_str = ('{0}/Secondary_STRUCT'.format(inside_dir))
+os.mkdir(sec_str)
+H = {}
+E = {}
+T = {}
+C = {}
+for item in fasta_files:
+	reitem = item.replace(".fasta","_fasta")
+	subprocess.call("garnier -sequence {0} -outfile {1}/{2}.garnier".format(item,sec_str,reitem), shell=True)
+second_files = os.listdir('{0}'.format(sec_str))
+print(second_files)
+for item in second_files:
+	keys = item.replace("_fasta.garnier","")
+	sec_file = open("{0}/{1}".format(sec_str,item)).read().rstrip()
+	m = re.search(r"percent: H: (\d+.\d+) E: (\d+.\d+) T: (\d+.\d+) C: (\d+.\d+)",sec_file)
+	H[keys] = m.group(1)
+	E[keys] = m.group(2)
+	T[keys] = m.group(3)
+	C[keys] = m.group(4)
+
+S = pd.DataFrame([H,E,T,C])
+S.plot.barh()
+plt.show()
+print(S)
+#S.to_csv("secondary.txt", sep="\t")
